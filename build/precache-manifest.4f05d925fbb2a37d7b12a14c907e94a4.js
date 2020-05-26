02cbab53f7be7a81e7cfcd4373fcb8222189d3a9
Vlad <bamper25@gmail.com>
2020-05-26 17:31:21 +0300

    Add build

---
+++ build/precache-manifest.4f05d925fbb2a37d7b12a14c907e94a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4fdb048aa3854622fdb",
+    "url": "/static/css/main.48bef788.chunk.css"
+  },
+  {
+    "revision": "e4fdb048aa3854622fdb",
+    "url": "/static/js/main.27cf27da.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9f85bc508667e40d94b7",
+    "url": "/static/js/2.e6f0371d.chunk.js"
+  },
+  {
+    "revision": "71c1eeecbd52e3319f59ebb2cf1bbc6f",
+    "url": "/index.html"
+  }
+];
